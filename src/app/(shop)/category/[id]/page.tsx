060f99e1b3a1895a5f25bd677d4bb401efa7f69f
Diana Pinilla <dianii230416@gmail.com>
2025-06-26 17:12:35 -0500

    feat: Create category page and filter by category

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,21 +1,36 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 
-interface Props{
-params:{
-  id:string
-}
+interface Props {
+  params: {
+    id: Category;
+  };
 }
 
-export default function({params}:Props) {
+export default function ({ params }: Props) {
+  const { id } = params;
 
-  const {id} = params;
+  const labels: Record<Category,string> = {
+    'men': 'Hombres',
+    'women':'Mujeres',
+    'kid':'Niños',
+    'unisex':'Todos'
+  }
 
-if(id === 'kids'){
-  notFound();
-}
+  const productsByCategory = initialData.products.filter(
+    (item) => item.gender === id
+  );
+
+  // if(id === 'kids'){
+  //   notFound();
+  // }
+
   return (
     <div>
-      <h1>category page {id}</h1>
+      <Title title={`Productos para ${labels[id]}`} subtitle={`Productos para ${labels[id]}`} />
+      <ProductGrid products={productsByCategory} />
     </div>
   );
 }
