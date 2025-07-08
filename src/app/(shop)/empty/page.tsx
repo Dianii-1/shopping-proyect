e3909d015331f113ffc0933b01a12cbdc2cc788e
Diana Pinilla <dianii230416@gmail.com>
2025-07-08 15:48:52 -0500

    feat: Create empty page

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+import { IoCartOutline } from "react-icons/io5"
 
-export default function EmptyPage() {
+export default function () {
   return (
-    <div>
-      <h1>empty page</h1>
+    <div className="flex gap-5 justify-center items-center h-[800px]">
+      <IoCartOutline size={80} />
+      <div className="flex flex-col gap-2 items-center">
+        <h1 className="text-xl font-semibold">Tu carrito está vacío</h1>
+        <Link href={'/'} className="text-blue-500 text-4xl">Regresar</Link>
+      </div>
     </div>
   );
 }
