d73589b204641121e035eaf4820a0a44a05eb542
Diana Pinilla <dianii230416@gmail.com>
2025-07-08 15:33:18 -0500

    feat: Create page list of orders

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html className="h-full" lang="es">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
