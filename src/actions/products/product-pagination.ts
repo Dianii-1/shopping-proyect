15d9e98f6fbba265ae9310a1aab1784bf646b1fb
Diana Pinilla <dianii230416@gmail.com>
2025-07-22 15:30:34 -0500

    feat: Create function get all products

---
+++ src/actions/products/product-pagination.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+
+export const getPaginatedProductsWithImages = async () => {
+  try {
+    const products = await prisma.product.findMany({
+      include: {
+        ProductImage: {
+          take: 2,
+          select: {
+            url: true,
+          },
+        },
+      },
+    });
+  } catch (error) {}
+};
