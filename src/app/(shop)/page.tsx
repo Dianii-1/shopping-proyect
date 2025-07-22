45e13aa2c0a7e4b8bc8295df8cb975d03724b73f
Diana Pinilla <dianii230416@gmail.com>
2025-07-22 16:46:10 -0500

    refactor: controll when dont have products

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,6 +1,7 @@
 import { getPaginatedProductsWithImages } from "@/actions";
 import { ProductGrid } from "@/components";
 import { Title } from "@/components/ui/title/Title";
+import { redirect } from "next/navigation";
 
 interface Props {
   searchParams: {
@@ -12,6 +13,10 @@ export default async function ({ searchParams }: Props) {
   const page = searchParams.page ? parseInt(searchParams.page) : 1;
 
   const { products } = await getPaginatedProductsWithImages({ page });
+
+  if (products.length === 0) {
+    redirect("/");
+  }
 
   return (
     <>
