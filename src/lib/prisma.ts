d6a08232d4c0a0babf53618b3aa2cfc184f827f5
Diana Pinilla <dianii230416@gmail.com>
2025-07-29 18:50:04 -0500

    refactor: prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,29 @@
 import { PrismaClient } from "../generated/prisma";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+// const prismaClientSingleton = () => {
+//   return new PrismaClient();
+// };
 
 // const prisma = new PrismaClient();
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+// type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
-};
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClientSingleton | undefined;
+// };
+// const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+// export default prisma;
+
+// if (process.env.NODE_ENV === "production") globalForPrisma.prisma = prisma;
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma = global.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
 
 export default prisma;
-
-if (process.env.NODE_ENV === "production") globalForPrisma.prisma = prisma;
