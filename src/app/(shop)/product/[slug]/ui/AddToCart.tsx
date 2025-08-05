2d1a6dcb74b381363ddd714620313b324a3b0f5e
Diana Pinilla <dianii230416@gmail.com>
2025-08-05 16:53:47 -0500

    feat: Add error message

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -9,8 +9,18 @@ interface Props {
 export const AddToCart = ({ product }: Props) => {
   const [size, setSize] = useState<Size | undefined>();
   const [quantity, setQuantity] = useState<number>(1);
+  const [posted, setPosted] = useState<boolean>(false);
+
+  const addToCart = () => {
+    setPosted(true);
+  };
   return (
     <>
+      {posted && !size && (
+        <span className="mt-2 text-red-500 fade-in">
+          Debe seleccionar una talla*
+        </span>
+      )}
       {/* selector de tallas */}
 
       <SizeSelector
@@ -23,7 +33,9 @@ export const AddToCart = ({ product }: Props) => {
       <QuantitySelector quantity={quantity} onQuantityChange={setQuantity} />
 
       {/* boton */}
-      <button className="btn-primary my-5">Agregar al carrito</button>
+      <button className="btn-primary my-5" onClick={addToCart}>
+        Agregar al carrito
+      </button>
     </>
   );
 };
