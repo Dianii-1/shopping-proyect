88a688d7350e621a90195c49c169672908a9b760
Diana Pinilla <dianii230416@gmail.com>
2025-09-13 09:25:20 -0500

    feat: Show user role

--- src/app/(shop)/profile/page.tsx
+++ src/app/(shop)/profile/page.tsx
@@ -12,6 +12,7 @@ export default async function ProfilePage() {
     <div>
       <Title title="Perfil" />
       <pre>{JSON.stringify(session.user, null, 2)}</pre>
+      <p className="text-5xl mt-4">{session.user.role}</p>
     </div>
   );
 }
