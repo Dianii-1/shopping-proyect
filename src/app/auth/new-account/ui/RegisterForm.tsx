f60e091ddf61d4d2227867847e2ddce58aa49281
Diana Pinilla <dianii230416@gmail.com>
2025-09-15 17:31:09 -0500

    refactor: Create component registerForm

---
+++ src/app/auth/new-account/ui/RegisterForm.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import Link from "next/link";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type FormInputs = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+export const RegisterForm = () => {
+  const { register, handleSubmit } = useForm<FormInputs>();
+
+  const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+    const { email, name, password } = data;
+    console.log({ email, name, password });
+    // server actions
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+      <label htmlFor="email">Nombre completo</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="text"
+        {...register("name", { required: true })}
+      />
+
+      <label htmlFor="email">Correo electrónico</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="email"
+        {...register("email", { required: true })}
+      />
+
+      <label htmlFor="email">Contraseña</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="email"
+        {...register("password", { required: true })}
+      />
+
+      <button className="btn-primary">Crear cuenta</button>
+
+      {/* divisor l ine */}
+      <div className="flex items-center my-5">
+        <div className="flex-1 border-t border-gray-500"></div>
+        <div className="px-2 text-gray-800">O</div>
+        <div className="flex-1 border-t border-gray-500"></div>
+      </div>
+
+      <Link href="/auth/login" className="btn-secondary text-center">
+        Ingresar
+      </Link>
+    </form>
+  );
+};
