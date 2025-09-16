100e762c34968c5531a6b05d10ab0c375844f073
Diana Pinilla <dianii230416@gmail.com>
2025-09-16 17:31:48 -0500

    feat: Create function login

--- src/actions/auth/login.ts
+++ src/actions/auth/login.ts
@@ -1,6 +1,7 @@
 "use server";
 import { AuthError } from "next-auth";
 import { signIn } from "@/auth";
+import { redirect } from "next/dist/server/api-utils";
 
 export async function authenticate(
   prevState: string | undefined,
@@ -23,5 +24,17 @@ export async function authenticate(
       }
     }
     throw error;
+  }
+}
+
+export async function login(email: string, password: string) {
+  try {
+    await signIn("credentials", { email, password, redirect: false });
+  } catch (error) {
+    console.log(error);
+    return {
+      ok: false,
+      message: "No se pudo iniciar sesión",
+    };
   }
 }
