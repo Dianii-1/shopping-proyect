321b30e0c0fb756506fd386de52a3c4c36827376
Diana Pinilla <dianii230416@gmail.com>
2025-09-16 17:05:27 -0500

    feat: Create function create user

---
+++ src/actions/auth/register.ts
@@ -0,0 +1,34 @@
+"use server";
+import bcryptjs from "bcryptjs";
+
+export const RegisterUser = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+  try {
+    const user = await prisma?.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: bcryptjs.hashSync(password),
+      },
+      select: {
+        name: true,
+        email: true,
+        id: true,
+      },
+    });
+
+    return {
+      ok: true,
+      user: user,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      ok: false,
+      messaje: "No se pudo crear el usuario",
+    };
+  }
+};
