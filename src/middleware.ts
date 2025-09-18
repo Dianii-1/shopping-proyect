92ef0c843200b5da0be05c656173a014878224b6
Diana Pinilla <dianii230416@gmail.com>
2025-09-18 15:22:37 -0500

    feat: Add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { auth } from "@/auth"; // Tu archivo auth.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const session = await auth();
+  const isLoggedIn = !!session?.user;
+  const pathname = request.nextUrl.pathname;
+
+  const protectedRoutes = [
+    "/checkout/address",
+    "/admin",
+    "/orders",
+    "/profile",
+  ];
+
+  const isProtected = protectedRoutes.some(
+    (route) => pathname === route || pathname.startsWith(route + "/")
+  );
+
+  // const isProtectedRoute =
+  //   request.nextUrl.pathname.startsWith("/checkout/address");
+
+  if (isProtected && !isLoggedIn) {
+    const loginUrl = new URL("/auth/login", request.url);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/checkout/:path*", "/admin", "/orders/:path*", "/profile"],
+};
