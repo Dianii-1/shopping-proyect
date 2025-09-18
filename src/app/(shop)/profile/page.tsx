8549556d29a8dcfb26e6cc2978fc34c411cdea3a
Diana Pinilla <dianii230416@gmail.com>
2025-09-18 15:24:16 -0500

    refactor: Add protected routes

--- src/app/(shop)/profile/page.tsx
+++ src/app/(shop)/profile/page.tsx
@@ -1,18 +1,14 @@
 import { auth } from "@/auth";
 import { Title } from "@/components";
-import { redirect } from "next/navigation";
 
 export default async function ProfilePage() {
   const session = await auth();
 
-  if (!session?.user) {
-    redirect("/");
-  }
   return (
     <div>
       <Title title="Perfil" />
-      <pre>{JSON.stringify(session.user, null, 2)}</pre>
-      <p className="text-5xl mt-4">{session.user.role}</p>
+      <pre>{JSON.stringify(session!.user, null, 2)}</pre>
+      <p className="text-5xl mt-4">{session!.user.role}</p>
     </div>
   );
 }
