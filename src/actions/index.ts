4512cf812ae97c9b4a65ec8f64217800b6a208ed
Diana Pinilla <dianii230416@gmail.com>
2025-09-20 09:38:32 -0500

    feat: Create server action getCountries

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,9 @@
 export * from "./products/product-pagination";
 export * from "./products/get-product-by-slug";
 export * from "./products/get-stock-by-slug";
+
 export * from "./auth/login";
 export * from "./auth/logout";
 export * from "./auth/register";
+
+export * from "./country/get-country";
