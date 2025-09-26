30f12072edf069ada778e55d71d2460b3389d397
Diana Pinilla <dianii230416@gmail.com>
2025-09-25 21:17:59 -0500

    refactor: update seed

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -15,6 +15,11 @@ async function main() {
   // con productos y categoria
 
   // Promise.all([
+
+  await prisma.orderAddress.deleteMany();
+  await prisma.orderItem.deleteMany();
+  await prisma.order.deleteMany();
+
   await prisma.userAddress.deleteMany();
   await prisma.user.deleteMany();
   await prisma.country.deleteMany();
