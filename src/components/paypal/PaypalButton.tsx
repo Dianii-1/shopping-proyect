8b6f1a71db70e1c062986b704a612bca13a0d695
Diana Pinilla <dianii230416@gmail.com>
2025-10-16 18:50:53 -0500

    feat: Add skeleton in button

--- src/components/paypal/PaypalButton.tsx
+++ src/components/paypal/PaypalButton.tsx
@@ -1,6 +1,16 @@
 "use client";
-import { PayPalButtons } from "@paypal/react-paypal-js";
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
 
 export const PaypalButton = () => {
+  const [{ isPending }] = usePayPalScriptReducer();
+
+  if (isPending) {
+    return (
+      <div className="flex flex-col gap-4 animate-pulse mb-12">
+        <div className="h-11 bg-gray-300 rounded" />
+        <div className="h-11 bg-gray-300 rounded" />
+      </div>
+    );
+  }
   return <PayPalButtons />;
 };
