a7f345b13482516d139f79505a4491a571b192d1
Diana Pinilla <dianii230416@gmail.com>
2025-10-20 16:43:08 -0500

    feat: Create paypal interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,4 @@
 export * from "./product.interfaces";
 export * from "./country.interfaces";
 export * from "./address.interface";
+export * from "./paypal.interface";
---
+++ src/interfaces/paypal.interface.ts
@@ -0,0 +1,30 @@
+export interface PaypalOrderStatusResponse {
+  id: string;
+  intent: string;
+  status: string;
+  purchase_units: PurchaseUnit[];
+  create_time: Date;
+  links: Link[];
+}
+
+export interface Link {
+  href: string;
+  rel: string;
+  method: string;
+}
+
+export interface PurchaseUnit {
+  reference_id: string;
+  amount: Amount;
+  payee: Payee;
+}
+
+export interface Amount {
+  currency_code: string;
+  value: string;
+}
+
+export interface Payee {
+  email_address: string;
+  merchant_id: string;
+}
