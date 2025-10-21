bbe366f1d4fdf9e7a76024c822596600e5047eaf
Diana Pinilla <dianii230416@gmail.com>
2025-10-21 17:17:06 -0500

    refactor: shange style

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -6,7 +6,7 @@ export default function ShopLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="max-h-screen">
+    <main className="min-h-screen">
       <TopMenu />
       <Sidebar />
       <div className="p-0 sm:px-5">{children}</div>
