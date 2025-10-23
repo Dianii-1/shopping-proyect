82c3dcfa01e71767c2a0586e80283dda6e9b9681
Diana Pinilla <dianii230416@gmail.com>
2025-10-23 13:27:29 -0500

    feat: Create user interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,4 @@ export * from "./product.interfaces";
 export * from "./country.interfaces";
 export * from "./address.interface";
 export * from "./paypal.interface";
+export * from "./user.interface";
