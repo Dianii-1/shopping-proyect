6cdb6014f684602834b06f3f9e582842400baf1f
Diana Pinilla <dianii230416@gmail.com>
2025-10-25 08:59:22 -0500

    feat: Protected routes admin

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,7 @@ export async function middleware(request: NextRequest) {
   const session = await auth();
   const isLoggedIn = !!session?.user;
   const pathname = request.nextUrl.pathname;
+  const role = session?.user.role;
 
   const protectedRoutes = [
     "/checkout/address",
@@ -14,10 +15,15 @@ export async function middleware(request: NextRequest) {
     "/profile",
   ];
 
+  const protectedRoutesAdmin = ["/admin"];
+
   const isProtected = protectedRoutes.some(
     (route) => pathname === route || pathname.startsWith(route + "/")
   );
 
+  const isAdmin = protectedRoutesAdmin.some(
+    (route) => pathname === route || pathname.startsWith(route + "/")
+  );
   // const isProtectedRoute =
   //   request.nextUrl.pathname.startsWith("/checkout/address");
 
@@ -26,9 +32,14 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(loginUrl);
   }
 
+  if (role !== "admin" && isAdmin) {
+    const loginUrl = new URL("/auth/login", request.url);
+    return NextResponse.redirect(loginUrl);
+  }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/checkout/:path*", "/admin", "/orders/:path*", "/profile"],
+  matcher: ["/checkout/:path*", "/admin/:path*", "/orders/:path*", "/profile"],
 };
