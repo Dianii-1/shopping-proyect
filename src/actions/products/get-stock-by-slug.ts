d5287110b97a3739e59f1acd32a9be87a84d1673
Diana Pinilla <dianii230416@gmail.com>
2025-10-28 13:16:40 -0500

    feat: Add importation prisma

--- src/actions/products/get-stock-by-slug.ts
+++ src/actions/products/get-stock-by-slug.ts
@@ -1,4 +1,7 @@
 "use server";
+
+import prisma from "@/lib/prisma";
+
 export const getStockBySlug = async (slug: string): Promise<number> => {
   try {
     const stock = await prisma?.product.findFirst({
