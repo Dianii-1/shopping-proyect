2832feb128e50a0afe46382ea8920672c92978f3
Diana Pinilla <dianii230416@gmail.com>
2025-10-28 13:17:09 -0500

    feat: Create interface categories

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -3,3 +3,4 @@ export * from "./country.interfaces";
 export * from "./address.interface";
 export * from "./paypal.interface";
 export * from "./user.interface";
+export * from "./categories.interface";
