81dac34ef8845e2bc75d6122a9f78c6c8f6e47fc
Diana Pinilla <dianii230416@gmail.com>
2025-10-28 13:16:56 -0500

    refactor: Add promise to get service

--- src/app/(shop)/admin/product/[slug]/page.tsx
+++ src/app/(shop)/admin/product/[slug]/page.tsx
@@ -1,17 +1,20 @@
-import { getProductBySlug } from "@/actions";
+import { getCategories, getProductBySlug } from "@/actions";
 import { Title } from "@/components";
 import { redirect } from "next/navigation";
 import { ProductForm } from "./ui/ProductForm";
 
 interface Props {
-  params: {
+  params: Promise<{
     slug: string;
-  };
+  }>;
 }
 export default async function ProductPage({ params }: Props) {
-  const { slug } = params;
+  const { slug } = await params;
 
-  const product = await getProductBySlug(slug);
+  const [product, categories] = await Promise.all([
+    getProductBySlug(slug),
+    getCategories(),
+  ]);
 
   //   Todo: new product
   if (!product) {
@@ -23,7 +26,7 @@ export default async function ProductPage({ params }: Props) {
   return (
     <>
       <Title title={title} />
-      <ProductForm product={product} />
+      <ProductForm product={product} categories={categories} />
     </>
   );
 }
