a93a70b316f09a7c25cc92cbea72e4d6d63fde44
Diana Pinilla <dianii230416@gmail.com>
2025-11-18 13:28:08 -0500

    refactor: optimized middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,17 @@
-import { auth } from "@/auth"; // Tu archivo auth.ts
+// import { auth } from "@/auth"; // Tu archivo auth.ts
+import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const session = await auth();
-  const isLoggedIn = !!session?.user;
+  // const session = await auth();
+  // const isLoggedIn = !!session?.user;
+  const token = await getToken({
+    req: request,
+    secret: process.env.AUTH_SECRET,
+  });
   const pathname = request.nextUrl.pathname;
-  const role = session?.user.role;
+  // const role = session?.user.role;
 
   const protectedRoutes = [
     "/checkout/address",
@@ -27,12 +32,12 @@ export async function middleware(request: NextRequest) {
   // const isProtectedRoute =
   //   request.nextUrl.pathname.startsWith("/checkout/address");
 
-  if (isProtected && !isLoggedIn) {
+  if (isProtected && !token) {
     const loginUrl = new URL("/auth/login", request.url);
     return NextResponse.redirect(loginUrl);
   }
 
-  if (role !== "admin" && isAdmin) {
+  if (token?.role !== "admin" && isAdmin) {
     const loginUrl = new URL("/auth/login", request.url);
     return NextResponse.redirect(loginUrl);
   }
