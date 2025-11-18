c6443fe3985b8ff5551853b6a0db11caa9e596e4
Diana Pinilla <dianii230416@gmail.com>
2025-11-18 12:12:22 -0500

    refactor: Change export prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -32,10 +32,18 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const getPrisma = () => {
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = new PrismaClient();
+  }
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+  return globalForPrisma.prisma;
+};
 
-export default prisma;
+// const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = prisma;
+// }
+
+// export default prisma;
